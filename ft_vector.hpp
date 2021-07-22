4139b8d675cbad63f8728eedda1e3024c318b260
Mikhail-U-K <super.mishakurban@yandex.ru>
2021-07-22 18:54:42 +0300

    beginning

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -1,6 +1,11 @@
 #ifndef UNTITLED_FT_VECTOR_HPP
 #define UNTITLED_FT_VECTOR_HPP
 
-
+namespace ft {
+	template<class T>
+	class vector {
+	
+	};
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+	std::cout << "Hello, World!" << std::endl;
+	std::vector<int>	vec;
+	return 0;
 }
