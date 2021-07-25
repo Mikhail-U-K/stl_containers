33056d54d66849c2a63137657ce499a93850370a
Mikhail-U-K <programming2020>
2021-07-25 15:54:37 +0300

    last deafult values

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 
+
 namespace ft {
 	template<class T, typename Allocator = std::allocator<T> >
 	class vector {
@@ -13,7 +14,8 @@ namespace ft {
 		typedef	typename allocator_type::const_reference	const_reference;
 		typedef	typename allocator_type::pointer			pointer;
 		typedef	typename allocator_type::const_pointer		const_pointer;
-		typedef
+		typedef	ptrdiff_t									difference_type;
+		typedef	size_t										size_type;
 	};
 }
 
