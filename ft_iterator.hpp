188c37328aaa6c3d1cb00efb55f1ab9b25b0665b
Mikhail-U-K <super.mishakurban@yandex.ru>
2021-07-26 17:18:56 +0300

    continue with iterator creation

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -5,17 +5,17 @@ namespace vector_iterator {
 	template<typename T>
 	class ft_iterator {
 	public:
-		typedef	T							value_type;
-		typedef	std::ptrdiff_t				difference_type;
-		typedef	value_type*					pointer;
-		typedef	value_type&					reference;
-		typedef	std::random_access_iterator	iterator_category;
+		typedef	T								value_type;
+		typedef	std::ptrdiff_t					difference_type;
+		typedef	value_type*						pointer;
+		typedef	value_type&						reference;
+		typedef	std::random_access_iterator_tag	iterator_category;
 	private:
 		pointer		m_ptr;
 	public:
 		explicit ft_iterator(pointer ptr) : m_ptr(ptr) {};
 		~ft_iterator() {};
-		ft_iterator(const ft_iterator &copy) { *this = copy };
+		ft_iterator(const ft_iterator &copy) { *this = copy ;};
 		ft_iterator& operator= (const ft_iterator &iter)
 		{
 			if (this == &iter)
@@ -23,6 +23,29 @@ namespace vector_iterator {
 			m_ptr = iter.m_ptr;
 			return (*this);
 		}
+		ft_iterator& operator++ () {++m_ptr; return *this;};
+		ft_iterator operator++ (int ) {
+			ft_iterator tmp(m_ptr);
+			++m_ptr;
+			return (tmp);
+		};
+		ft_iterator& operator-- () {--m_ptr; return *this;};
+		ft_iterator operator-- (int ) {
+			ft_iterator	tmp(m_ptr);
+			++m_ptr;
+			return (tmp);
+		};
+		ft_iterator& operator+= (difference_type value) { m_ptr += value; return *this;};
+		ft_iterator& operator-= (difference_type value) { m_ptr -= value; return *this;};
+		reference operator[] (difference_type value) {return *(m_ptr + value);};
+		
+		bool operator== (const ft_iterator &other) {return (m_ptr = other.m_ptr);};
+		bool operator!= (const ft_iterator &other) {return (m_ptr != other.m_ptr);}
+		reference operator* () {return *m_ptr;};
+		reference operator-> () {return *m_ptr;};
+		
+//		pointer operator* () {return m_ptr;};
+//		pointer operator-> () {return m_ptr;};
 	};
 }
 
--- ft_vector.hpp
+++ ft_vector.hpp
@@ -2,6 +2,7 @@
 #define UNTITLED_FT_VECTOR_HPP
 
 #include <memory>
+#include "ft_iterator.hpp"
 
 
 namespace ft {
@@ -16,7 +17,7 @@ namespace ft {
 		typedef	typename allocator_type::const_pointer		const_pointer;
 		typedef	std::ptrdiff_t								difference_type;
 		typedef	size_t										size_type;
-		
+		typedef	vector_iterator::ft_iterator<value_type>	iterator;
 	};
 }
 
