00db0f690275b93c47f2e46dcda5ebeacbe80669
Mikhail-U-K <programming2020>
2021-07-27 20:50:57 +0300

    iterator and const iterator finished

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -2,6 +2,10 @@
 #define UNTITLED_FT_ITERATOR_HPP
 
 namespace vector_iterator {
+
+	template<typename T>
+	class	ft_const_iterator;
+
 	template<typename T>
 	class ft_iterator {
 	public:
@@ -10,6 +14,7 @@ namespace vector_iterator {
 		typedef	value_type*						pointer;
 		typedef	value_type&						reference;
 		typedef	std::random_access_iterator_tag	iterator_category;
+		typedef	ft_const_iterator<value_type>	const_iterator;
 	private:
 		pointer		m_ptr;
 	public:
@@ -17,11 +22,20 @@ namespace vector_iterator {
 		ft_iterator() : m_ptr(nullptr) {}
 		~ft_iterator() {}
 		ft_iterator(const ft_iterator &copy) {*this = copy;}
+		ft_iterator(const const_iterator &copy) {*this = copy;}
+
 		ft_iterator& operator= (const ft_iterator &iter)
 		{
 			if (this == &iter)
 				return (*this);
 			m_ptr = iter.m_ptr;
+			return (*this);
+		}
+		ft_iterator& operator= (const const_iterator &iter)
+		{
+			if (this == &iter)
+				return (*this);
+			m_ptr = &(*iter);
 			return (*this);
 		}
 		ft_iterator& operator++ () {++m_ptr; return *this;};
@@ -36,8 +50,8 @@ namespace vector_iterator {
 			++m_ptr;
 			return (tmp);
 		}
-		ft_iterator& operator+= (difference_type value) { m_ptr += value; return *this;}
-		ft_iterator& operator-= (difference_type value) { m_ptr -= value; return *this;}
+		ft_iterator& operator+= (difference_type value) {m_ptr += value; return *this;}
+		ft_iterator& operator-= (difference_type value) {m_ptr -= value; return *this;}
 		ft_iterator operator- (difference_type n) const {return ft_iterator(m_ptr - n);}
 		ft_iterator operator+ (difference_type n) const {return ft_iterator(m_ptr + n);}
 
@@ -51,7 +65,16 @@ namespace vector_iterator {
 		bool operator>= (const ft_iterator &ref) const {return m_ptr >= ref.m_ptr;}
 		bool operator< (const ft_iterator &ref) const {return m_ptr < ref.m_ptr;}
 		bool operator<= (const ft_iterator &ref) const {return m_ptr <= ref.m_ptr;}
+
+		bool operator== (const const_iterator &other) {return (m_ptr = &(*other));}
+		bool operator!= (const const_iterator &other) {return (m_ptr != &(*other));}
+		bool operator> (const const_iterator &ref) const {return m_ptr > &(*ref);}
+		bool operator>= (const const_iterator &ref) const {return m_ptr >= &(*ref);}
+		bool operator< (const const_iterator &ref) const {return m_ptr < &(*ref);}
+		bool operator<= (const const_iterator &ref) const {return m_ptr <= &(*ref);}
 	};
+
+
 	template<typename T>
 	class ft_const_iterator {
 	public:
@@ -67,7 +90,10 @@ namespace vector_iterator {
 		explicit ft_const_iterator(pointer ptr) : const_ptr(ptr) {}
 		ft_const_iterator() : const_ptr(nullptr) {}
 		~ft_const_iterator() {};
+
 		ft_const_iterator(const ft_const_iterator &copy) {*this = copy;}
+		ft_const_iterator(const iterator &copy) {*this = copy;}
+
 		ft_const_iterator& operator= (const ft_const_iterator &iter)
 		{
 			if (this == &iter)
@@ -79,27 +105,33 @@ namespace vector_iterator {
 		{
 			if (this == &iter)
 				return (*this);
-			const_ptr = iter.const_ptr;
+			const_ptr = &(*iter);
 			return (*this);
 		}
-		ft_const_iterator& operator+= (difference_type value) {const_ptr += value; return *this;}
-		ft_const_iterator& operator-= (difference_type value) {const_ptr -= value; return *this;}
+
+		ft_const_iterator& operator+= (difference_type value) const {const_ptr += value; return *this;}
+		ft_const_iterator& operator-= (difference_type value) const {const_ptr -= value; return *this;}
 		ft_const_iterator operator- (difference_type n) const {return ft_const_iterator(const_ptr - n);}
 		ft_const_iterator operator+ (difference_type n) const {return ft_const_iterator(const_ptr + n);}
 
 		reference operator[] (difference_type value) {return *(const_ptr + value);}
-		reference operator* () {return *const_ptr;}
-		pointer operator-> () {return const_ptr;}
+		const reference operator* () {return *const_ptr;}
+		const pointer operator-> () {return const_ptr;}
 
-		bool operator== (const ft_const_iterator &other) {return (const_ptr = other.const_ptr);}
-		bool operator!= (const ft_const_iterator &other) {return (const_ptr != other.const_ptr);}
+		bool operator== (const ft_const_iterator &other) const {return (const_ptr = other.const_ptr);}
+		bool operator!= (const ft_const_iterator &other) const {return (const_ptr != other.const_ptr);}
 		bool operator> (const ft_const_iterator &ref) const {return const_ptr > ref.const_ptr;}
 		bool operator>= (const ft_const_iterator &ref) const {return const_ptr >= ref.const_ptr;}
 		bool operator< (const ft_const_iterator &ref) const {return const_ptr < ref.const_ptr;}
 		bool operator<= (const ft_const_iterator &ref) const {return const_ptr <= ref.const_ptr;}
 
+		bool operator== (const iterator &other) const {return (const_ptr = &(*other));}
+		bool operator!= (const iterator &other) const {return (const_ptr != &(*other));}
+		bool operator> (const iterator &ref) const {return const_ptr > &(*ref);}
+		bool operator>= (const iterator &ref) const {return const_ptr >= &(*ref);}
+		bool operator< (const iterator &ref) const {return const_ptr < &(*ref);}
+		bool operator<= (const iterator &ref) const {return const_ptr <= &(*ref);}
 	};
 }
-
 
 #endif
--- ft_vector.hpp
+++ ft_vector.hpp
@@ -8,15 +8,16 @@ namespace ft {
 	template<class T, typename Allocator = std::allocator<T> >
 	class vector {
 	public:
-		typedef	T											value_type;
-		typedef	Allocator									allocator_type;
-		typedef	typename allocator_type::reference			reference;
-		typedef	typename allocator_type::const_reference	const_reference;
-		typedef	typename allocator_type::pointer			pointer;
-		typedef	typename allocator_type::const_pointer		const_pointer;
-		typedef	std::ptrdiff_t								difference_type;
-		typedef	size_t										size_type;
-		typedef	vector_iterator::ft_iterator<value_type>	iterator;
+		typedef	T												value_type;
+		typedef	Allocator										allocator_type;
+		typedef	typename allocator_type::reference				reference;
+		typedef	typename allocator_type::const_reference		const_reference;
+		typedef	typename allocator_type::pointer				pointer;
+		typedef	typename allocator_type::const_pointer			const_pointer;
+		typedef	std::ptrdiff_t									difference_type;
+		typedef	size_t											size_type;
+		typedef	vector_iterator::ft_iterator<value_type>		iterator;
+		typedef	vector_iterator::ft_const_iterator<value_type>	const_iterator;
 	};
 }
 
